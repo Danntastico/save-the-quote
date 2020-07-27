4d731b2b6655f4201015c1886141cb4b5d4c9e60
Danilo Peña <nicolasdpf@gmail.com>
2020-07-27 11:08:03 -0500

    Configuring enhancers to set the Middleware for async actions

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { authReducer } from '../reducers/authReducer';
 
-//For multiple reducers
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
+//For multiple reducers
 const reducers = combineReducers({
   auth: authReducer,
 });
 
 export const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
